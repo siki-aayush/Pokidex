237a58a7075a08b81e6a35e8c141468fb16df28e
Siki Aayush <37694502+siki-aayush@users.noreply.github.com>
2021-08-21 20:09:32 +0545

    added documentation in csv to md convert file

--- convert_csv_to_md.py
+++ convert_csv_to_md.py
@@ -2,18 +2,30 @@ import csv
 
 pokedex_data = []
 with open("./pokedex.csv", "r", encoding="utf-8", errors="replace") as pokedex:
+    """
+    Converts the pokedex csv file to a markdown file
+    """
+
+    # Initializing reader
     reader = csv.reader(pokedex)
+
+    # Creating header format for markdown table
     labels  = next(reader)
     temp = ['---' for label in labels]
     pokedex_data.append(f"|{'|'.join(labels)}|")
     pokedex_data.append(f"|{'|'.join(temp)}|")
 
+    # Appending data with markdown code
     for row in reader:
-        print("row", row)
         pokedex_data.append(f"|{'|'.join(row)}|")
 
-    [print(t) for t in pokedex_data] 
+    # Displaying all the data format
+    for t in pokedex_data:
+        print(t)
 
 with open("./pokedex.md", "w", encoding="utf-8", errors="replace") as md:
+    """
+    Writing the markdown code in pokedex.md file
+    """
     for t in pokedex_data:
         md.write(t + '\n')
